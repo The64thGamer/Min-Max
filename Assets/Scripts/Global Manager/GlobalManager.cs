1a45dbbb65ca8a6200b230d369a62f08c2f459a9
The 64th Gamer <69170079+The64thGamer@users.noreply.github.com>
2023-05-28 06:36:47 -0500

    Fixed Desync Bug

--- Assets/Scripts/Global Manager/GlobalManager.cs
+++ Assets/Scripts/Global Manager/GlobalManager.cs
@@ -96,7 +96,6 @@ public class GlobalManager : NetworkBehaviour
                     };
                 }
                 SendPosClientRpc(playerPosRPCData.ToArray());
-                return;
             }
         }
         tickTimer += Time.deltaTime;
