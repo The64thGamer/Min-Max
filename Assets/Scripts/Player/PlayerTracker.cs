70d1acc1ff2d1519c9eff8618d17f836a3f8f303
The 64th Gamer <69170079+The64thGamer@users.noreply.github.com>
2023-07-03 19:11:19 -0500

    Fixed non owner clients lerping movement

--- Assets/Scripts/Player/PlayerTracker.cs
+++ Assets/Scripts/Player/PlayerTracker.cs
@@ -130,7 +130,7 @@ public class PlayerTracker : NetworkBehaviour
             }
         }
 
-        if (!IsHost)
+        if (!IsHost && IsOwner)
         {
             //Lerping client-side movement with server positions for a smoother experience
             transform.position = Vector3.Lerp(transform.position, predictedPos, Mathf.Clamp01(Vector3.Distance(transform.position, predictedPos) - 1));
